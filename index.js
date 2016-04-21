98305f11f761e68d7ee2a08ddd8e4ecc85a4ca01
backfrip <backfrip@users.noreply.github.com>
2016-04-21 03:45:37 +0000

    Well there goes the animation, but it comes back easily.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var canvas = document.getElementById('magic');
 var ctx = canvas.getContext('2d');
+
+canvas.addEventListener('click', function(e, x, y) {
+  var ball = makeBall(e.offsetX, e.offsetY, 50);
+  ball.draw();
+});
 ctx.fillStyle = '#000000';
 ctx.clear = function() { this.clearRect(0, 0, canvas.width, canvas.height); };
 
-var makeBall = function() {
-  var x = 250;
-  var y = 250;
-  var r = 50;
+var makeBall = function(x, y, r) {
+  var x = x;
+  var y = y;
+  var r = r;
   
-  var getx = function( ) { return x; };
-  var gety = function( ) { return y; };
-  var getr = function( ) { return r; };
-  var setx = function(n) { x = n; };
-  var sety = function(n) { y = n; };
-  var setr = function(n) { r = n; };
-  var draw = function( ) {
+  var getx = function(   ) { return x; };
+  var gety = function(   ) { return y; };
+  var getr = function(   ) { return r; };
+  var setx = function(n  ) { x = n; };
+  var sety = function(n  ) { y = n; };
+  var setr = function(n  ) { r = n; };
+  var incx = function(n=1) { x += n; };
+  var incy = function(n=1) { y += n; };
+  var incr = function(n=1) { r += n; };
+  var draw = function(   ) {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2);
     ctx.closePath();
@@ -28,15 +36,10 @@ var makeBall = function() {
     setx : setx,
     sety : sety,
     setr : setr,
+    incx : incx,
+    incy : incy,
+    incr : incr,
     draw : draw,
   };
 };
 
-var ball = makeBall();
-var animate = function() {
-  ctx.clear();
-  ball.draw();
-  window.requestAnimationFrame(animate);
-};
-
-animate();
